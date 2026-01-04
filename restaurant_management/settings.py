c22cc157e5f9624f96924d20edf727f0dc650215
224rifche <cherif2862@gmail.com>
2026-01-04 17:23:08 +0000

    changement de l'URL

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -16,7 +16,7 @@ CACHE_MIDDLEWARE_KEY_PREFIX = 'restaurant'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = [h.strip() for h in config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',') if h.strip()]
+ALLOWED_HOSTS = [h.strip() for h in config('ALLOWED_HOSTS', default='localhost,127.0.0.1,restaurant-django-production.up.railway.app',).split(',') if h.strip()]
 
 CSRF_TRUSTED_ORIGINS = [o.strip() for o in config('CSRF_TRUSTED_ORIGINS', default='').split(',') if o.strip()]
 
