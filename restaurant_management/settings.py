b370bbe3d54fe301dfa7a69fe8f2e9924df74395
224rifche <cherif2862@gmail.com>
2026-01-04 13:39:30 +0000

    Configuration pour le déploiement sur Render

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -129,7 +129,7 @@ TEMPLATES = [
 
 # Configuration des fichiers statiques et médias
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
