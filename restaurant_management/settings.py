1d24bf72f6733afa85286d6c1b313454cd07092d
224rifche <cherif2862@gmail.com>
2026-01-05 23:37:22 +0000

    fix: move AWS credentials to .env

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -62,13 +62,18 @@ INSTALLED_APPS = [
 # Configuration S3
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'mon-restaurant-media-2026'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('mon-restaurant-media-2026')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_DEFAULT_ACL = 'public-read'
+AWS_QUERYSTRING_AUTH = False  # Important pour les URLs publiques
+AWS_S3_REGION_NAME = 'us-east-1'  # Remplacez par votre région si différente
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_LOCATION = 'media'
+
+# Stockage des médias
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
